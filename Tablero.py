38e01415630387835f711ef96f562cea13a2d168
Ervin <ervinrodriguez140@gmail.com>
2022-01-26 21:36:09 -0600

    ada

--- Tablero.py
+++ Tablero.py
@@ -1,18 +1,16 @@
 def hola():
     print("Hola")
 
-<<<<<<< HEAD
 def hola():
 
     print("Hola")
 
 def hola():
     print("Hola")
-=======
+    
 def saludo():
     for i in range(100):
         print("Hola Eddy")
->>>>>>> prueba
 
 def hola():
     print("gfgfg")
